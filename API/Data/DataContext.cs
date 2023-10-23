547b4bbe02f0a55fc259fe62deab8717bff4f3c1
Karol <karolk1232.kk@gmail.com>
2023-10-23 08:57:12 +0200

    InitialCommit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+
+    public DataContext(DbContextOptions options): base(options)
+    {
+        
+    }
+
+    public DbSet<Patient> Patients {get; set;}
+
+}
---
+++ API/Entities/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API;
+
+public class Patient
+{
+
+    public int Id {get; set;}
+
+    public string firstName {get; set;}
+
+    public string lastName { get; set; }
+
+    [Phone]
+    public string phone {get; set;}
+
+}
