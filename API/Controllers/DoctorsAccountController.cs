ab60835c76de402dc7b89c15eff1199b941bab53
Kakayashi <limiksier@gmail.com>
2023-11-08 19:37:09 +0100

    patient & doctor validation

--- API/Controllers/DoctorsAccountController.cs
+++ API/Controllers/DoctorsAccountController.cs
@@ -1,10 +1,12 @@
 
 
+using System.ComponentModel.DataAnnotations;
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -16,23 +18,23 @@ namespace API.Controllers
     {
         private readonly ITokenService _tokenService;
         private readonly DataContext _context;
-        
+
         public DoctorsAccountController(DataContext context, ITokenService tokenService)
         {
             this._context = context;
             this._tokenService = tokenService;
-            
+
         }
 
         [HttpPost("register")]
         public async Task<ActionResult<DoctorDto>> Register(DoctorRegisterDto registerDto)
         {
-            
-            
-            if(await UserExists(registerDto.Email)) return BadRequest("User with that email already exists!"); 
-            if(await PWZTaken(registerDto.PWZ)) return BadRequest("Already taken PWZ");
-            if(!ValidPWZ(registerDto.PWZ)) return BadRequest("Invalid PWZ");
-            
+
+
+            if (await UserExists(registerDto.Email)) return BadRequest("User with that email already exists!");
+            if (await PWZTaken(registerDto.PWZ)) return BadRequest("Already taken PWZ");
+            if (!Validators.ValidatePWZ(registerDto.PWZ)) return BadRequest("Invalid PWZ");
+
             using var hmac = new HMACSHA512();
 
             var doctor = new Doctor
@@ -46,13 +48,13 @@ namespace API.Controllers
 
             _context.Doctors.Add(doctor);
             await _context.SaveChangesAsync();
-        
+
             return new DoctorDto
             {
                 Email = doctor.Email,
                 Token = _tokenService.CreateToken(doctor)
             };
-        
+
         }
 
         [HttpPost("login")]
@@ -66,9 +68,9 @@ namespace API.Controllers
 
             var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
 
-            for(int i=0; i<computedHash.Length; i++)
+            for (int i = 0; i < computedHash.Length; i++)
             {
-                if( computedHash[i] != doctor.PasswordHash[i]) return Unauthorized("Invalid email or password!");
+                if (computedHash[i] != doctor.PasswordHash[i]) return Unauthorized("Invalid email or password!");
             }
 
             return new DoctorDto
@@ -83,22 +85,6 @@ namespace API.Controllers
         public async Task<bool> UserExists(string email)
         {
             return await _context.Doctors.AnyAsync(x => x.Email.Equals(email.ToLower()));
-        }
-
-
-        public bool ValidPWZ(string pwz)
-        {
-            var suma = 0;
-            var temp = 0;
-
-            foreach(char c in pwz){
-                suma += (int) char.GetNumericValue(c) * temp;
-                temp++;
-            }
-            
-            if (suma % 11 == (int) char.GetNumericValue(pwz[0])) return true;
-
-            return false;
         }
 
         public async Task<bool> PWZTaken(string pwz)
---
+++ API/Helpers/Validators.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Helpers
+{
+    public class Validators
+    {
+
+        public static bool ValidatePWZ(string pwz)
+        {
+            if (pwz.Length != 7)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < 7; i++)
+            {
+                if (pwz[i] < '0' || pwz[i] > '9')
+                {
+                    return false;
+                }
+            }
+
+            int sum = 0;
+            for (int i = 1; i <= 6; i++)
+            {
+                int digit = pwz[i] - '0';
+                sum += digit * i;
+            }
+
+            int controlDigit = sum % 11;
+            int expectedControlDigit = pwz[0] - '0';
+
+            if (controlDigit == 10)
+            {
+                controlDigit = 0;
+            }
+
+            return controlDigit == expectedControlDigit;
+        }
+
+
+        public static bool ValidatePesel(string pesel)
+        {
+            if (pesel.Length != 11)
+            {
+                return false;
+            }
+
+            int[] weights = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (char.IsDigit(pesel[i]))
+                {
+                    int digit = int.Parse(pesel[i].ToString());
+                    sum += digit * weights[i];
+                }
+                else
+                {
+                    return false; // Nieprawidłowy znak
+                }
+            }
+
+            int controlDigit = 10 - (sum % 10);
+            if (controlDigit == 10)
+            {
+                controlDigit = 0;
+            }
+
+            int peselControlDigit;
+            if (int.TryParse(pesel[10].ToString(), out peselControlDigit))
+            {
+                if (controlDigit == peselControlDigit)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+}
